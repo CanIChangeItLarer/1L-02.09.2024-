66079a00cc90367ecd7845444836fff713809e12
Name Surname <your@email>
2024-09-12 18:22:13 +0300

    Итоги превой лекции

---
+++ ConsoleApplication1/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    public class Item
+    {
+        public Item()
+        {
+            var random = new Random();
+            Id = random.Next(0,10);
+        }
+
+        public Item(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+        //get; set; - аксессоры, методы доступа
+        public int Id { get; private set; }
+        public string Name { get; set; }
+
+        public virtual void SendNotification(string message) //виртуальные методы переопределяемы
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ ConsoleApplication1/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+//protected +наследоваться, -вызывать доступ к методу родительского класса, нотдальше по наследству нельзя переопределять
+namespace ConsoleApplication1
+{
+    public class Student : Item
+    {
+        public Student(int id, string name) : base(id,name) //наследование
+        {
+        }
+        public override void SendNotification(string message)
+        {
+            //base.SendNotification(message);
+            Console.WriteLine("Message from student: " + message);
+        }
+    }
+}
+//абстрактные классы. в них есть абстрактные методы, они не имеют реализации
+//класс потомок должен переопределить (реализовать) эти методы
+//например, сохранение данных (нет универсального варианта)
+//sealed - апрет наследования от класса
